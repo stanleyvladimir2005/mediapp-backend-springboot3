a368ac4fb42b329bfd2b40b7716762fe81fe7635
Vladimir Stanley Alvarez Alvarado <stanleyvladimir2005@hotmail.com>
2026-01-04 18:25:50 -0600

    Actualizando: 1-Spring boot 4.0.1

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM openjdk:25-ea-1-slim
 
 LABEL author=stanleyvladimir2005@gmail.com
 
-COPY "./build/libs/mediapp-backend-springboot3-0.0.1-SNAPSHOT.jar" "app.jar"
+COPY "./build/libs/mediapp-backend-springboot4-0.0.1-SNAPSHOT.jar" "app.jar"
 
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/com/mitocode/MediappBackendSpringboot4Application.java
+++ src/main/java/com/mitocode/MediappBackendSpringboot4Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MediappBackendSpringboot3Application {
+public class MediappBackendSpringboot4Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MediappBackendSpringboot3Application.class, args);
+        SpringApplication.run(MediappBackendSpringboot4Application.class, args);
     }
 }
